7e79bd6332dcc00621a7bacef4dafad4e01b5509
John Gomes <silva.johngomes@gmail.com>
2024-08-02 12:43:55 -0300

    request number descriptions

--- pedido/src/main/java/br/com/rappidu/infra/controllers/PaymentController.java
+++ pedido/src/main/java/br/com/rappidu/infra/controllers/PaymentController.java
@@ -3,6 +3,9 @@ package br.com.rappidu.infra.controllers;
 import br.com.rappidu.application.usecases.*;
 import br.com.rappidu.domain.entities.Payment;
 import br.com.rappidu.infra.controllers.dto.RequestPaymentUpdate;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
@@ -24,7 +27,11 @@ public class PaymentController {
     private final FindPaymentUseCase findPaymentUseCase;
     private final UpdatePaymentUseCase updatePaymentUseCase;
 
-
+    @Operation(summary = "WebHook para atualização de status de pagamento",
+            description = "Se o pagamento for concluido, o status do pedido também é atualizado",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "Detalhes do novo usuário",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = RequestPaymentUpdate.class))))
     @PatchMapping("/{code}")
     public ResponseEntity<?> update(@PathVariable Long code, @RequestBody RequestPaymentUpdate paymentUpdate) {
         Payment payment = new Payment(code, paymentUpdate.amount(), paymentUpdate.status(), paymentUpdate.type());
